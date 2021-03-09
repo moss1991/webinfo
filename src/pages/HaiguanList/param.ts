463e647c32c707ceb2e4efed5fe3df04f69b3e3d
carlos <419903711@qq.com>
2021-03-09 14:18:28 +0800

    +调整海关数据title 20210309 rc done

--- src/pages/HaiguanList/param.ts
+++ src/pages/HaiguanList/param.ts
@@ -1,3 +1,18 @@
+const IMPORT_OR_EXPORT = {
+  0 : '进口',
+  1 : '出口'
+}
+
+const CORD_TYPE = {
+  0 : '4位编码',
+  1 : '8位编码'
+}
+
+const MONTH_TYPE = {
+  0 : '当月值',
+  1 : '累计值'
+}
+
 const table_params = {
   columns : [
     {
@@ -7,120 +22,123 @@ const table_params = {
       width: 150,
     },
     {
-      title: 'product_id',
+      title: '商品编码',
       dataIndex: 'product_id',
       key: 'product_id',
       width: 150,
     },
     {
-      title: 'product_name',
+      title: '商品名称',
       dataIndex: 'product_name',
       key: 'product_name',
-      width: 150,
+      width: 400,
     },
     {
-      title: 'trade_country_id',
+      title: '贸易伙伴编码',
       dataIndex: 'trade_country_id',
       key: 'trade_country_id',
       width: 150,
     },
     {
-      title: 'trade_country_name',
+      title: '贸易伙伴名称',
       dataIndex: 'trade_country_name',
       key: 'trade_country_name',
       width: 150,
     },
     {
-      title: 'trade_id',
+      title: '贸易方式编码',
       dataIndex: 'trade_id',
       key: 'trade_id',
       width: 150,
     },
     {
-      title: 'trade_name',
+      title: '贸易方式名称',
       dataIndex: 'trade_name',
       key: 'trade_name',
       width: 150,
     },
 
     {
-      title: 'register_id',
+      title: '注册地编码',
       dataIndex: 'register_id',
       key: 'register_id',
       width: 150,
     },
 
     {
-      title: 'first_unit',
+      title: '第一计量单位',
       dataIndex: 'first_unit',
       key: 'first_unit',
       width: 150,
     },
     {
-      title: 'first_price',
+      title: '第一数量',
       dataIndex: 'first_price',
       key: 'first_price',
       width: 150,
     },
     {
-      title: 'second_unit',
+      title: '第二计量单位',
       dataIndex: 'second_unit',
       key: 'second_unit',
       width: 150,
     },
     {
-      title: 'second_price',
+      title: '第二数量',
       dataIndex: 'second_price',
       key: 'second_price',
       width: 150,
     },
     {
-      title: 'price',
+      title: '美元',
       dataIndex: 'price',
       key: 'price',
       width: 150,
     },
     {
-      title: 'year',
+      title: '年',
       dataIndex: 'year',
       key: 'year',
       width: 150,
     },
     {
-      title: 'start_month',
+      title: '开始月份',
       dataIndex: 'start_month',
       key: 'start_month',
       width: 150,
     },
     {
-      title: 'end_month',
+      title: '结束月份',
       dataIndex: 'end_month',
       key: 'end_month',
       width: 150,
     },
     {
-      title: 'import_type',
+      title: '进出口类型',
       dataIndex: 'import_type',
       key: 'import_type',
       width: 150,
+      render:(text:any, record:any) => IMPORT_OR_EXPORT[text]
     },
     {
-      title: 'code_type',
+      title: '编码类型',
       dataIndex: 'code_type',
       key: 'code_type',
       width: 150,
+      render:(text:any, record:any) => CORD_TYPE[text]
     },
     {
-      title: 'month_type',
+      title: '当月值/累计值',
       dataIndex: 'month_type',
       key: 'month_type',
       width: 150,
+      render:(text:any, record:any) => MONTH_TYPE[text]
     },
     {
-      title: 'create_time',
+      title: '数据采集时间',
       dataIndex: 'create_time',
       key: 'create_time',
-      width: 150,
+      width: 300,
     },
   ],
   // page_size : 50,
